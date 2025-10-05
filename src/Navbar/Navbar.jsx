b58cc1cb79eaf6e34b699580e4b34fc8748ecbfb
GabOnTrash <gabrielearmenise08@gmail.com>
2025-10-05 18:49:15 +0200

    Changes t othe navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -23,7 +23,12 @@ function Navbar()
                 <li><a href="#about">About me</a></li>
                 <li><a href="#skills">Dev skills</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a href="/Gabriele_Armenise_CV_IT_ENG.zip" download>Curriculum</a></li>
+                <li><a href="/Gabriele_Armenise_CV_IT_ENG.zip" download>
+                    Curriculum
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.3rem" viewBox="0 -960 960 960" width="1.3rem" fill="currentColor">
+                        <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/>
+                    </svg>
+                </a></li>
             </ul>
         </nav>
     );
